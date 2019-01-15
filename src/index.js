30c8939d481e648612b09e9f004db1e0d09d315c
sarahyjja <sarahkharraz@gmail.com>
2019-01-15 16:04:41 +0000

    Show London 5 day forecast on the page

--- src/index.js
+++ src/index.js
@@ -10,5 +10,22 @@ todayWeather();
 
 async function fourDaysWeather(){
   const fourDaysTemp = await weather.forecast()
-  document.getElementById('london_fourDaysTemp').innerHTML = date();
+  const londonFourDaysTemp = document.getElementById('london_fourDaysTemp')
+  fourDaysTemp.forEach((item) => {
+    const threeHoursBloc = document.createElement('div');
+    const temp = document.createElement('p');
+    const description = document.createElement('p');
+    const dtTxt = document.createElement('p');
+
+    temp.innerHTML = item.temp;
+    description.innerHTML = item.description;
+    dtTxt.innerHTML = item.dt_txt;
+
+    threeHoursBloc.appendChild(temp)
+    threeHoursBloc.appendChild(description)
+    threeHoursBloc.appendChild(dtTxt)
+
+    londonFourDaysTemp.appendChild(threeHoursBloc)
+  });
 }
+fourDaysWeather();
